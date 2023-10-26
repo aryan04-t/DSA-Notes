220a98e54d230f8f2a856a5311ebffed5cc81172
Aryan Tomar <aryantomar5000@gmail.com>
2023-10-26 23:06:30 +0530

    Updated "Numeric Half Pyramid Pattern"

--- 02_Playing with Patterns/11_Numeric Half Pyramid/01_Numeric_Half_Pyramid.cpp
+++ 02_Playing with Patterns/11_Numeric Half Pyramid/01_Numeric_Half_Pyramid.cpp
@@ -1,4 +1,4 @@
-// Numeric Half Pyramid 
+// Numeric Half Pyramid (Type-1) 
 
 #include<iostream> 
 using namespace std;
@@ -6,16 +6,16 @@ using namespace std;
 int main(){
 
     cout << endl; 
-    cout << "\"To Print Numeric Half Pyramid Pattern\"" << endl; 
+    cout << "\"To Print Numeric Half Pyramid Pattern (Type-1)\"" << endl; 
     cout << endl; 
 
     int height; 
-    cout << "Enter the height of \"Numeric Half Pyramid\": "; 
+    cout << "Enter the height of \"Numeric Half Pyramid (Type-1)\": "; 
     cin >> height; 
 
     cout << endl; 
 
-    // Logic for printing "Numeric Half Pyramid": 
+    // Logic for printing "Numeric Half Pyramid" (Type-1): 
     for(int row=0; row < height; row++){
         for(int col=0; col < row+1; col++){
             cout << col+1 << " ";   
@@ -30,8 +30,7 @@ int main(){
 
 
 /*
-    // My Different Logic for Printing "Numeric Half Pyramid": 
-
+    // My Different Logic for Printing "Numeric Half Pyramid (Type-1)": 
     for(int row=0; row < height; row++){
 
         int count=1;
@@ -43,4 +42,42 @@ int main(){
 
         cout << endl;
     }
+*/
+
+
+// _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+
+/*
+
+// Numeric Half Pyramid (Type-2) 
+
+#include<iostream> 
+using namespace std;
+
+int main(){
+
+    cout << endl; 
+    cout << "\"To Print Numeric Half Pyramid Pattern (Type-2)\"" << endl; 
+    cout << endl; 
+
+    int height; 
+    cout << "Enter the height of \"Numeric Half Pyramid (Type-2)\": "; 
+    cin >> height; 
+
+    cout << endl; 
+
+    // Logic for printing "Numeric Half Pyramid (Type-2)": 
+    for(int row=0; row < height; row++){
+        for(int col=0; col < row+1; col++){
+            cout << row + col + 1 << " ";   
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    
+    return 0;
+}
+
 */
