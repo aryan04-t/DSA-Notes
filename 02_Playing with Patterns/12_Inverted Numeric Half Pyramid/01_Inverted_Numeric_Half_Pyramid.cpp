85a0a014d012a9fb67dc7406f849d0e50c342634
Aryan Tomar <aryantomar5000@gmail.com>
2023-10-26 23:09:44 +0530

    Updated "Inverted Numeric Half Pyramid Pattern"

--- 02_Playing with Patterns/12_Inverted Numeric Half Pyramid/01_Inverted_Numeric_Half_Pyramid.cpp
+++ 02_Playing with Patterns/12_Inverted Numeric Half Pyramid/01_Inverted_Numeric_Half_Pyramid.cpp
@@ -1,4 +1,4 @@
-// Inverted Numeric Half Pyramid
+// Inverted Numeric Half Pyramid (Type-1) 
 
 #include<iostream> 
 using namespace std;
@@ -6,16 +6,16 @@ using namespace std;
 int main(){
 
     cout << endl;
-    cout << "\"To Print Inverted Numeric Half Pyramid Pattern\"" << endl;
+    cout << "\"To Print Inverted Numeric Half Pyramid Pattern (Type-1)\"" << endl;
     cout << endl;
 
     int height; 
-    cout << "Enter the height of \"Inverted Numeric Half Pyramid\": "; 
+    cout << "Enter the height of \"Inverted Numeric Half Pyramid (Type-1)\": "; 
     cin >> height; 
 
     cout << endl;
 
-    // Logic for printing "Inverted Numeric Half Pyramid": 
+    // Logic for printing "Inverted Numeric Half Pyramid (Type-1)": 
     for(int row=0; row < height; row++){
         for(int col=0; col < height-row; col++){
             cout << col+1 << " ";
@@ -30,7 +30,7 @@ int main(){
 
 
 /*
-    // My Different Logic for Printing "Inverted Numeric Half Pyramid": 
+    // My Different Logic for Printing "Inverted Numeric Half Pyramid (Type-1)": 
     
     for(int row=0; row < height; row++){
         
@@ -43,4 +43,44 @@ int main(){
 
         cout << endl;
     }
+*/
+
+
+// _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+
+/*
+
+// Inverted Numeric Half Pyramid (Type-2) 
+
+#include<iostream> 
+using namespace std;
+
+int main(){
+
+    cout << endl;
+    cout << "\"To Print Inverted Numeric Half Pyramid Pattern (Type-2)\"" << endl;
+    cout << endl;
+
+    int height; 
+    cout << "Enter the height of \"Inverted Numeric Half Pyramid (Type-2)\": "; 
+    cin >> height; 
+
+    cout << endl;
+
+    int count=1;
+
+    // Logic for printing "Inverted Numeric Half Pyramid (Type-2)": 
+    for(int row=0; row < height; row++){
+        for(int col=0; col < height-row; col++){
+            cout << row + col + 1 << " ";
+        }
+        cout << endl; 
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
 */
