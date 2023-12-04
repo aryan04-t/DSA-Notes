c296e3015916047af8f67f73dd7eece44f75ab97
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-04 22:58:21 +0530

    Find Maximum Number in an Array

---
+++ 05_1D Arrays/08_Find_Maximum_Number_In_An_Array.cpp
@@ -0,0 +1,16 @@
+// Finding Maximum Number in an Array using Linear Search 
+
+// include header file <limits.h> fo using INT_MIN
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // it's a good practice to initialize maxNum variable with INT_MIN while finding the maximum number 
+    // and same goes for finding minimum number, it's a good practice to initialize minNum variable with INT_MAX while finding the maximum number 
+
+
+
+    return 0;
+}
